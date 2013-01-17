c03bed7d4385b6b03c512e8dd00e63741fedfe31
Aaron Wishnick <awishnick@server.fake>
2013-01-17 16:10:26 -0500

    Refactoring. No functionality change.

--- intercept-xcodebuild.py
+++ intercept-xcodebuild.py
@@ -1,25 +1,46 @@
 #!/usr/bin/env python
+"""Build a compilation database in JSON format, suitable for clang.
+
+This is done by intercepting the compiler, and running xcodebuild.
+"""
+
 import os
 import os.path as path
 from subprocess import Popen, PIPE
 import sys
 
-script_path = path.dirname(os.path.realpath(__file__))
-intercept_path = path.join(script_path, 'intercept.py')
-command_list_path = path.join(os.getcwd(), 'compile_commands.json')
 
-with open(command_list_path, 'w') as f:
-    f.write('[\n')
+def intercept_xcodebuild(xcodebuild_args, intercept_path, command_list_path):
+    """Run xcodebuild, substituting intercept.py for the compiler.
 
-new_env = dict(os.environ,
-               INTERCEPT_COMMAND_LIST=command_list_path,
-               CC=intercept_path,
-               CXX=intercept_path)
+    intercept.py will write the commands out to command_list_path.
+    """
+    with open(command_list_path, 'w') as f:
+        f.write('[\n')
 
-args = list(sys.argv)
-args[0] = 'xcodebuild'
-p = Popen(args, env=new_env, stdin=PIPE, stdout=PIPE)
-(stdoutdata, stderrdata) = p.communicate()
+    new_env = dict(os.environ,
+                   INTERCEPT_COMMAND_LIST=command_list_path,
+                   CC=intercept_path,
+                   CXX=intercept_path)
 
-with open(command_list_path, 'a') as f:
-    f.write(']')
+    args = list(xcodebuild_args)
+    args.insert(0, 'xcodebuild')
+    p = Popen(args, env=new_env, stdin=PIPE, stdout=PIPE)
+    p.communicate()
+
+    with open(command_list_path, 'a') as f:
+        f.write(']')
+
+
+def main(args):
+    script_path = path.dirname(os.path.realpath(__file__))
+    intercept_path = path.join(script_path, 'intercept.py')
+    command_list_path = path.join(os.getcwd(), 'compile_commands.json')
+
+    intercept_xcodebuild(args[1:],
+                         intercept_path,
+                         command_list_path)
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
