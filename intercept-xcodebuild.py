1b0c7f3ca142164ce327b84b8d048483f0e3c139
Aaron Wishnick <awishnick@server.fake>
2013-01-17 16:38:09 -0500

    This utility now supports merging compilation databases into a master file.

--- intercept-xcodebuild.py
+++ intercept-xcodebuild.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 """Build a compilation database in JSON format, suitable for clang.
 
-This is done by intercepting the compiler, and running xcodebuild.
+This is done by intercepting the compiler, and running xcodebuild. The first
+argument passed in is the (possibly existing) compilation database to merge the
+new results with. All remaining arguments are passed to xcodebuild.
 """
 
 import os
 import os.path as path
 from subprocess import Popen, PIPE
 import sys
+import json
+from shutil import copyfile
 
 
 def intercept_xcodebuild(xcodebuild_args, intercept_path, command_list_path):
@@ -28,8 +32,46 @@ def intercept_xcodebuild(xcodebuild_args, intercept_path, command_list_path):
     p = Popen(args, env=new_env, stdin=PIPE, stdout=PIPE)
     p.communicate()
 
-    with open(command_list_path, 'a') as f:
-        f.write(']')
+    # intercept.py doesn't actually parse or write JSON, so it'll end up with a
+    # trailing comma and newline. Remove the comma but keep the newline.
+    with open(command_list_path, 'r') as f:
+        contents = f.read()
+    contents = contents[:-2] + '\n]'
+    with open(command_list_path, 'w') as f:
+        f.write(contents)
+
+
+def build_command_dict(commands):
+    """Take a list of command objects, and make a dict keyed by filename."""
+    return {command['file']: command for command in commands}
+
+
+def merge_compile_commands(new_commands, existing):
+    """Take an existing list of commands, and a new list, and merge them.
+
+    Commands are merged as dictionaries keyed by the filenames. Files that do
+    not already exist are added, and files that already existed are updated
+    with the new directory/command.
+    """
+    existing_dict = build_command_dict(existing)
+    existing_dict.update(build_command_dict(new_commands))
+    return [command for _, command in existing_dict.iteritems()]
+
+JSON_OPTIONS = {
+    'indent': 2,
+    'separators': (',', ': '),
+}
+
+
+def update_merged_command_list(command_list_path, merged_path):
+    """Load the new and existing command lists, and merge to the existing one.
+    """
+    with open(command_list_path, 'r') as command_list_file, \
+            open(merged_path, 'r') as merged_file:
+        merged = merge_compile_commands(json.loads(command_list_file.read()),
+                                        json.loads(merged_file.read()))
+    with open(merged_path, 'w') as merged_file:
+        merged_file.write(json.dumps(merged, **JSON_OPTIONS))
 
 
 def main(args):
@@ -37,10 +79,16 @@ def main(args):
     intercept_path = path.join(script_path, 'intercept.py')
     command_list_path = path.join(os.getcwd(), 'compile_commands.json')
 
-    intercept_xcodebuild(args[1:],
+    merged_path = args[1]
+
+    intercept_xcodebuild(args[2:],
                          intercept_path,
                          command_list_path)
 
+    if path.exists(merged_path):
+        update_merged_command_list(command_list_path, merged_path)
+    else:
+        copyfile(command_list_path, merged_path)
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv))
